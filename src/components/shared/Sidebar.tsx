980fac013e5d56af2cb930d7222a2fc8326d1721
aliahasan <aanabin18@gmail.com>
2025-02-12 19:46:24 +0600

    updated

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,13 +1,29 @@
 import Link from "next/link";
+import {
+  FaHome,
+  FaInbox,
+  FaPen,
+  FaProjectDiagram,
+  FaTachometerAlt,
+} from "react-icons/fa";
 
 const Sidebar = () => {
   const links = [
-    { label: "Home", href: "/" },
-    { label: "Inbox", href: "/dashboard/all-messages" },
-    { label: "Add Blog", href: "/dashboard/add-blog" },
-    { label: "All Blogs", href: "/dashboard/all-blogs" },
-    { label: "Add Projects", href: "/dashboard/add-project" },
-    { label: "All Projects", href: "/dashboard/all-projects" },
+    { label: "Home", href: "/", icon: <FaHome /> },
+    { label: "Dashboard", href: "/dashboard", icon: <FaTachometerAlt /> }, // Added Dashboard icon here
+    { label: "Inbox", href: "/dashboard/all-messages", icon: <FaInbox /> },
+    { label: "Add Blog", href: "/dashboard/add-blog", icon: <FaPen /> },
+    { label: "All Blogs", href: "/dashboard/all-blogs", icon: <FaPen /> },
+    {
+      label: "Add Projects",
+      href: "/dashboard/add-project",
+      icon: <FaProjectDiagram />,
+    },
+    {
+      label: "All Projects",
+      href: "/dashboard/all-projects",
+      icon: <FaProjectDiagram />,
+    },
   ];
 
   return (
@@ -18,8 +34,10 @@ const Sidebar = () => {
             <Link href={link.href} key={link.label}>
               <p
                 className="p-3 text-lg font-medium bg-gray-200 dark:bg-my-bg text-my-light dark:text-my-dark rounded-md cursor-pointer 
-                transition duration-300"
+                transition duration-300 flex items-center gap-3"
               >
+                {/* Display the icon alongside the text */}
+                {link.icon}
                 {link.label}
               </p>
             </Link>
